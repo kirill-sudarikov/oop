f3a5c2086c336eabb4883f035cdb6b8eef82d481
kirill-sudarikov <kirill.s.07@inbox.ru>
2026-05-31 13:58:15 +0400

    Refactired .gitignore

--- Practices/10-sfml/main.cpp
+++ Practices/10-sfml/main.cpp
@@ -1,15 +1,12 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
+int main() {
   sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
 
-  while (window.isOpen())
-  {
-    while (const std::optional event = window.pollEvent())
-    {
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
       if (event->is<sf::Event::Closed>())
         window.close();
     }
