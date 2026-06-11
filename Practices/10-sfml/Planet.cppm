60d65aa8be6044e9b3c16b3d6c10349cd8b3b601
kirill-sudarikov <kirill.s.07@inbox.ru>
2026-06-11 13:43:49 +0400

    Practice 10 complete

--- Practices/10-sfml/Planet.cppm
+++ Practices/10-sfml/Planet.cppm
@@ -7,20 +7,21 @@ export module Planet;
 import std;
 
 constexpr float G = 6.67e-11f;
+constexpr std::size_t MAX_TRAIL_SIZE = 25000;
 
 export class Planet {
 private:
   sf::CircleShape _shape;
   float _mass = 0.0f;
 
-  sf::VertexArray _trail{sf::PrimitiveType::LineStrip};
+  std::vector<sf::Vertex> _trail;
 
 public:
   sf::Vector2f _position;
   sf::Vector2f _velocity;
 
-  Planet(float m, float r, const sf::Vector2f &pos, const sf::Vector2f &v,
-         const sf::Color &color)
+  Planet(float m, float r, const sf::Vector2f& pos, const sf::Vector2f& v,
+         const sf::Color& color)
       : _mass(m), _position(pos), _velocity(v) {
 
     _shape.setRadius(r);
@@ -31,7 +32,7 @@ public:
 
   float mass() const { return _mass; }
 
-  void update(const Planet &other, float dt) {
+  void update(const Planet& other, float dt) {
     sf::Vector2f direction = other._position - _position;
     float r_sq = direction.x * direction.x + direction.y * direction.y;
 
@@ -51,22 +52,28 @@ public:
 
     _shape.setPosition(_position);
 
-    _trail.append(sf::Vertex(_position));
+    _trail.push_back(sf::Vertex(_position, _shape.getFillColor()));
 
-    if (_trail.getVertexCount() > 500) {
-      for (std::size_t i = 0; i < 50; ++i) {
-        _trail[i].color = sf::Color(15, 15, 30);
-      }
+    if (_trail.size() > MAX_TRAIL_SIZE) {
+      _trail.erase(_trail.begin());
+    }
 
-      for (std::size_t i = 1; i < _trail.getVertexCount(); ++i) {
-        _trail[i - 1] = _trail[i];
+    if (!_trail.empty()) {
+      std::size_t size = _trail.size();
+
+      for (std::size_t i = 0; i < size; ++i) {
+        float ratio = static_cast<float>(i) / size;
+
+        _trail[i].color.a = ratio * 255.0f;
       }
-      _trail.resize(_trail.getVertexCount() - 1);
     }
   }
 
-  void draw(sf::RenderWindow &window) {
-    window.draw(_trail);
+  void draw(sf::RenderWindow& window) {
+    if (!_trail.empty()) {
+      window.draw(_trail.data(), _trail.size(), sf::PrimitiveType::LineStrip);
+    }
+
     window.draw(_shape);
   }
 };
--- Practices/10-sfml/main.cpp
+++ Practices/10-sfml/main.cpp
@@ -8,13 +8,37 @@ int main() {
   sf::RenderWindow window(sf::VideoMode(window_scale), "Gravity Simulator");
 
   sf::Vector2f sun_pos({window_scale.x / 2.0f, window_scale.y / 2.0f});
-  Planet sun(3.5e16f, 30.0f, sun_pos, {0.0f, 0.0f}, sf::Color::Black);
+  Planet sun(3.5e15f, 30.0f, sun_pos, {0.0f, 0.0f}, sf::Color::Yellow);
 
-  sf::Vector2f earth_pos(sun_pos.x - 100.0f, sun_pos.y - 450.0f);
-  Planet earth(10.0e4f, 10.0f, earth_pos, {35.0f, 20.0f}, sf::Color::Blue);
+  sf::Vector2f mercury_pos(sun_pos.x, sun_pos.y - 60.0f);
+  sf::Color mercury_color(100, 100, 100);
+  Planet mercury(600.0f, 4.0f, mercury_pos, {65.0f, 0.0f}, mercury_color);
 
-  sf::Vector2f mars_pos(sun_pos.x - 40.0f, sun_pos.y - 450.0f);
-  Planet mars(5.0e4f, 7.0f, mars_pos, {30.0f, 15.0f}, sf::Color::Red);
+  sf::Vector2f venus_pos(sun_pos.x, sun_pos.y - 120.0f);
+  sf::Color venus_color(255, 255, 200);
+  Planet venus(3.5e4f, 9.0f, venus_pos, {45.0f, 0.0f}, venus_color);
+
+  sf::Vector2f earth_pos(sun_pos.x, sun_pos.y - 180.0f);
+  Planet earth(3.5e4f, 10.0f, earth_pos, {35.0f, 0.0f}, sf::Color::Blue);
+
+  sf::Vector2f mars_pos(sun_pos.x, sun_pos.y - 220.0f);
+  Planet mars(1.0e4f, 7.0f, mars_pos, {33.0f, 0.0f}, sf::Color::Red);
+
+  sf::Vector2f jupiter_pos(sun_pos.x, sun_pos.y - 345.0f);
+  sf::Color jupiter_color(255, 200, 150);
+  Planet jupiter(350.0e4f, 15.0f, jupiter_pos, {25.5f, 0.0f}, jupiter_color);
+
+  sf::Vector2f saturn_pos(sun_pos.x, sun_pos.y - 410.0f);
+  sf::Color saturn_color(210, 200, 175);
+  Planet saturn(150.0e4f, 13.5f, saturn_pos, {23.5f, 0.0f}, saturn_color);
+
+  sf::Vector2f uranus_pos(sun_pos.x, sun_pos.y - 475.0f);
+  sf::Color uranus_color(175, 220, 245);
+  Planet uranus(70.0e4f, 8.0f, uranus_pos, {21.8f, 0.0f}, uranus_color);
+
+  sf::Vector2f neptune_pos(sun_pos.x, sun_pos.y - 535.0f);
+  sf::Color neptune_color(64, 224, 208);
+  Planet neptune(70.0e4f, 8.0f, neptune_pos, {21.0f, 0.0f}, neptune_color);
 
   sf::Clock clock;
 
@@ -27,13 +51,25 @@ int main() {
     float dt = clock.restart().asSeconds();
     // if (dt > 0.02f) dt = 0.02f;
 
+    mercury.update(sun, dt);
+    venus.update(sun, dt);
     earth.update(sun, dt);
     mars.update(sun, dt);
+    jupiter.update(sun, dt);
+    saturn.update(sun, dt);
+    uranus.update(sun, dt);
+    neptune.update(sun, dt);
 
     window.clear(sf::Color(15, 15, 30));
     sun.draw(window);
+    venus.draw(window);
+    mercury.draw(window);
     earth.draw(window);
     mars.draw(window);
+    jupiter.draw(window);
+    saturn.draw(window);
+    uranus.draw(window);
+    neptune.draw(window);
     window.display();
   }
 }
